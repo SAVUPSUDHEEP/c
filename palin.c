3e115fefef1b62493970c7ed645b42cbce6f20ee
Savu P Sudheep <98030490+SAVUPSUDHEEP@users.noreply.github.com>
2023-08-12 15:14:15 +0530

    Add files via upload

---
+++ palin.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+ int reverse(int a)
+ {
+ int rev=0,r,i;
+ while(a>0)
+ {
+  r=a%10;
+  rev=(rev*10)+r;
+  a=a/10; 
+ }
+ return rev;
+ 
+}
+void main()
+{ 
+ int rev,r,n,arr[20],i,j,p,flag;
+ printf("\nenter the number:");
+ scanf("%d",&n);
+ printf("\nenter the elements of array:");
+ for(i=0;i<n;i++)
+ { 
+  scanf("%d",&arr[i]);
+ }
+
+ for(i=0;i<n;i++)
+ {
+  int temp=arr[i];
+ int p=reverse(arr[i]);
+ if(p==temp)
+ {
+  flag++;
+ }
+ }
+ if(n==flag)
+ {
+ printf("palin array:");
+ }
+ else
+  {
+  printf("\nnot a palinarray");
+  }
+  }
